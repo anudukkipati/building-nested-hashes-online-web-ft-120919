a939cca1353594eab2d1ede64fdaa01b49e1fd3b
Anupama Gadde <anupamagadde@yahoo.com>
2019-12-13 13:51:35 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,9 +2,17 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_Friends => {}
       }, 
     :capulet => {
       #your key/value pairs here!
+      :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_Friends => {}
       }}
 
   
